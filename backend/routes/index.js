16daa30b2126e7dc8d063a2feed5ae4bbf6e658a
fabipv <fabiana.pina.v@mail.pucv.cl>
2025-06-06 21:37:14 -0400

    feat(backend): implement EP 2.2 - database configuration and relational modeling

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
 
+const db = new sqlite3.Database('./database/database');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Expresso' });
+});
+
+/* GET /ping route. */
+router.get('/ping', function(req, res, next) {
+   db.all(`SELECT name FROM sqlite_master WHERE type='table'`, [], (err, tables) => {
+    if (err) {
+      console.error('Error consultando tablas:', err);
+      return res.status(500).json({ error: 'Error al obtener las tablas' });
+    }
+    console.log('Tablas existentes:', tables);
+    
+    db.all('SELECT * FROM usuarios', [], (err, rows) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error al obtener los usuarios' });
+      }
+      res.json(rows);
+    });
+  });
 });
 
 module.exports = router;
